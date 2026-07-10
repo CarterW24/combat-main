0afa02144415c23e2dbd4bf7d44c0b984baba9cd
JadenY <jadeny43571@gmail.com>
2026-07-10 15:21:25 -0500

    webapi fix
    (cherry picked from commit 92624fdc5d730d54b896e5ff4969d75e4e925999)

--- src/Sanctuary.WebAPI/Program.cs
+++ src/Sanctuary.WebAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -19,6 +20,8 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("database.json", optional: true);
 
 builder.WebHost.UseUrls();
 
